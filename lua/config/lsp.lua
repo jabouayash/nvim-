df2a16fad720e40fb49fe9cad3a0f0febe1ee801
Jacob AbouAyash <jaabou@amazon.com>
2025-12-10 05:29:20 +0000

    fix: disable csharp lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -229,12 +229,7 @@ local servers = {
   },
 
   -- C# / .NET
-  omnisharp = {
-    cmd = { "omnisharp" },
-    enable_roslyn_analyzers = true,
-    organize_imports_on_format = true,
-    enable_import_completion = true,
-  },
+  csharp_ls = {},
 
   -- Java
   jdtls = {
